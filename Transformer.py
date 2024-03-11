00ef14ae6518d5cefe0d167b9e129f1b563f35f3
Miguel Ong (@ahalation) <agitatehalation@gmail.com>
2024-03-11 13:53:25 +0800

    gitignore

--- Transformer.py
+++ Transformer.py
@@ -8,3 +8,4 @@ script_dir = os.path.dirname(__file__)
 
 df_train = pd.read_csv(os.path.join(script_dir, r"RECCON-main/data/transform/train.csv"))
 df_test = pd.read_csv(os.path.join(script_dir, r"RECCON-main/data/transform/test.csv"))
+
--- tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-print(df_train["emotion"].value_counts(ascending=True))
