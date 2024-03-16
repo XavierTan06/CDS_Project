c3dfa8ba4cc81d695d3e61723cce1ecc36c7b072
XavierTan06 <126039477+XavierTan06@users.noreply.github.com>
2024-03-16 18:43:14 +0800

    Multilayer perceptron

---
+++ EDFnlp_MLP.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import classification_report
+from sklearn.neural_network import MLPClassifier
+from labelMap import id2label
+
+#Load data
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+trainset = pd.read_csv(os.path.join(dir_path, r"data/clean/EDFnlp_train.csv"))
+testset = pd.read_csv(os.path.join(dir_path, r"data/clean/EDFnlp_test.csv"))
+
+trainsetX = trainset["text"]
+testsetX = testset["text"]
+trainsetY = trainset["labels"].replace(id2label)
+testsetY = testset["labels"].replace(id2label)
+
+#Tokenize text as n-grams and convert them into vectors
+vectorizer = TfidfVectorizer(ngram_range=(1, 2))
+trainVecX = vectorizer.fit_transform(trainsetX)
+testVecX = vectorizer.transform(testsetX)
+
+#Build and run MLP
+mlp_model = MLPClassifier(hidden_layer_sizes=(10,), activation='relu', solver='adam', max_iter=100, verbose=True)
+mlp_model.fit(trainVecX, trainsetY)
+
+#Evaluate
+y_pred = mlp_model.predict(testVecX)
+print("Classification Report:")
+print(classification_report(testsetY, y_pred))
